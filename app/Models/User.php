7e5a5074d6574329763bfb83330292b7762b8062
1277663638 <1277663638@qq.com>
2020-07-30 14:12:05 +0000

    Move user model to models folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -18,6 +18,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password',
     ];
+
+    protected $table = 'users';
 
     /**
      * The attributes that should be hidden for arrays.
